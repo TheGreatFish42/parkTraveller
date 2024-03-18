11775a54a5f76344c6db5569885cab13f1fd38ac
jacob fisher <jacobcfisher0@gmail.com>
2024-03-18 16:31:00 -0400

    updated for deployment

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+export default About 
+
+function About(){
+    return(
+        <div className="about">
+            <p>Welcome! This site had been developed to help explorers find their next great adventure. Simply select your desired state and then browse the list of parks in that state. You can even view more details about each park by clicking on their name.</p>
+            <p>Currently our lists only contain National Parks and Monuments. More features may be added soon, but in the meantime just bear with us!</p>
+        </div>
+    )
+}
